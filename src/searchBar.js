1b5f33af1d2e4508f1e70052747813de4988a256
benedekmolnar <molnar.benedek@gmail.com>
2022-06-28 13:22:40 +0200

    Implement basic logic behind the search bar

--- src/searchBar.js
+++ src/searchBar.js
@@ -1,13 +1,24 @@
 import './searchBar.css';
 import {TextField} from "@mui/material";
 import LoadingButton from "@mui/material/Button";
+import {useState} from "react";
 
 function SearchBar(){
+    const [userInput, setUserInput] = useState("");
+    const [searchCriteria, setSearchCriteria] = useState("");
+
+    const handleChange = (e) => {
+        setUserInput(e.target.value);
+    }
+
+    const handleClick = () => {
+        setSearchCriteria(userInput);
+    }
 
     return(
         <div className="container">
-            <TextField fullWidth label="Search for a movie">SearchBar</TextField>
-            <LoadingButton variant="outlined">Submit</LoadingButton>
+            <TextField onChange={handleChange} fullWidth label="Search for a movie">SearchBar</TextField>
+            <LoadingButton onClick={handleClick} variant="outlined">Submit</LoadingButton>
         </div>
     )
 }
