4224a2edf6f34fbe4b1766009186601975d7a02e
benedekmolnar <molnar.benedek@gmail.com>
2022-06-29 15:39:44 +0200

    Implement displaying the results

--- src/searchBar.js
+++ src/searchBar.js
@@ -1,36 +1,69 @@
 import './searchBar.css';
-import {TextField} from "@mui/material";
+import {TextField, TableHead, Table, TableCell, TableRow, TableBody} from "@mui/material";
 import LoadingButton from "@mui/material/Button";
 import {useState} from "react";
 import {apiPost} from "./utility";
 
 function SearchBar(){
     const [userInput, setUserInput] = useState("");
+    const [data, setData] = useState("");
 
     const handleChange = (e) => {
         setUserInput(e.target.value);
     }
 
-    const handleClick = () => {
+    async function handleClick(){
         let query = `
             query SearchMovies {
             searchMovies(query: "${userInput}") {
-                    id
                     name
                     score
                     genres {name}
                   }
                 }
              `
-        apiPost("https://tmdb.sandbox.zoosh.ie/dev/graphql", query).then(result => console.log(result))
+        return await apiPost("https://tmdb.sandbox.zoosh.ie/dev/graphql", query).then(result => setData(result.data.searchMovies))
     }
 
     return(
-        <div className="container">
-            <TextField onChange={handleChange} fullWidth label="Search for a movie">SearchBar</TextField>
-            <LoadingButton onClick={handleClick} variant="outlined">Submit</LoadingButton>
+        <div className="main-container">
+            <div className="search-container">
+                <TextField onChange={handleChange} fullWidth label="Search for a movie">SearchBar</TextField>
+                <LoadingButton onClick={handleClick} variant="outlined">Submit</LoadingButton>
+            </div>
+            <div className="display-container">
+                <DisplayBar result={data}/>
+            </div>
         </div>
     )
+}
+
+function DisplayBar({result}){
+    if (result){
+        return(
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Score</TableCell>
+                        <TableCell>Category</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {result.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell>{row.name}</TableCell>
+                            <TableCell>{row.score}</TableCell>
+                            <TableCell>{row.genres.map(function(genre, index) {
+                                return <span key={index}>{ (index ? ', ' : '') + genre.name }</span>;
+                                })}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        )
+    }
 }
 
 export default SearchBar;
